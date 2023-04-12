78bfe53d1f1d50278dfde540f66ce9adb0aa1e24
Joshua Mir <joshuam10o@gmail.com>
2023-04-12 12:27:37 +0200

    wasm build and native build more separate

--- wasm-logic/xtask/src/main.rs
+++ wasm-logic/xtask/src/main.rs
@@ -6,9 +6,11 @@ fn main() -> Result<()> {
     .dist_dir_path("./dist")
     .app_name("wasm-logic");
     dist.release = true;
+    dist.no_default_features = true;
+    dist.features = vec!["wasm".to_string()];
     dist.run("catalyst-wasm-logic")?;
-    WasmOpt::level(1)
-    .shrink(2)
+    WasmOpt::level(4)
+    .shrink(4)
     .optimize("./dist/wasm-logic.wasm")?;
     Ok(())
 }
