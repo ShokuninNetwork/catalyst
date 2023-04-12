c95943b33899e420b33ff08ee42004a912f2fbf7
Joshua Mir <joshuam10o@gmail.com>
2023-04-12 11:36:43 +0200

    fix: no longer delete entire project on build also: recover older version of js - need to rewrite again :/

--- wasm-logic/xtask/src/main.rs
+++ wasm-logic/xtask/src/main.rs
@@ -3,7 +3,7 @@ use xtask_wasm::{Dist, WasmOpt};
 
 fn main() -> Result<()> {
     let mut dist = Dist::default()
-    .dist_dir_path("./")
+    .dist_dir_path("./dist")
     .app_name("wasm-logic");
     dist.release = true;
     dist.run("catalyst-wasm-logic")?;
